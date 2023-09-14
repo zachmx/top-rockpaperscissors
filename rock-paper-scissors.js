fe0d4a7d5be08b208f8e45ad3d68bd5e9c1f9e0d
Zach Miller <zach.mx@protonmail.com>
2023-09-14 16:22:36 -0700

    rock case

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,13 +1,30 @@
 
-function getComputerChoice(min, max){
+function computerChoice(min, max){
 
     let choice = Math.floor(Math.random() * (max - min) + min)
 
-    return choice == 1 ? 'Rock'
-        : choice == 2 ? 'Paper'
-        : choice == 3 ? 'Scissors'
+    return choice == 1 ? 'rock'
+        : choice == 2 ? 'paper'
+        : choice == 3 ? 'scissors'
         : "It's Rock, Paper, Scissors... Pick One!";
 
 }
 
-console.log(getComputerChoice(1, 4));
+function playerChoice() {
+    let choice = prompt("Rock, Paper, or Scissors?", " ");
+    let choiceLower = choice.toLowerCase();
+    return choiceLower;
+}
+
+function playRound(playerChoice, computerChoice) {
+
+    if (playerChoice == 'rock' && computerChoice == 'paper'){
+        alert("Computer picked Paper, You Lost!");
+    } else if (playerChoice == 'rock' && computerChoice == "scissors") {
+        alert ("Computer picked Scissors.  You win!");
+    } else if (playerChoice == 'rock' && computerChoice == 'rock') {
+        alert("Computer picked Rock.  You tied!");
+    }
+}
+
+playRound(playerChoice(), computerChoice(1, 4));
