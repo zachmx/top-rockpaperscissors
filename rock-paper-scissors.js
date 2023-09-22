5b7b86544ff31feeedeec377162ca9e21988ec23
Zach Miller - Laptop <zach.mx@protonmail.com>
2023-09-22 12:51:17 -0700

    scoreboard working

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -36,7 +36,7 @@ function playRound(playerChoice, computerChoice) {
                         } 
             } else if (playerChoice == "scissors"){
                 switch (computerChoice){
-                    case 'Rock':
+                    case 'rock':
                         return "loss"
                         break;
                     case 'scissors':
@@ -50,26 +50,11 @@ function playRound(playerChoice, computerChoice) {
             }
 } 
 
-function printResults(inputMatchResult) {
-    if (inputMatchResult == "win") {
-        console.log("You won this Round!")
-        playerWins++;
-    } else if (inputMatchResult == "loss") {
-        console.log("The computer won this Round!")
-        computerWins++;
-    } else if (inputMatchResult == "tie") {
-        console.log("You tied this Round!")
-    }
-
-    console.log("Player wins: " + playerWins);
-    console.log("Computer wins: " + computerWins);
-}
-
 function createPlayerOptions(){
 
-    var playerWins = 0;
-    var computerWins = 0;
-    var gameOver = false;
+    let playerWins = 0;
+    let computerWins = 0;
+    let gameOver = false;
 
     const playersZone = document.getElementById('userchoice')
 
@@ -95,68 +80,130 @@ function createPlayerOptions(){
     playersZone.appendChild(paperBtn);
     playersZone.appendChild(scissorsBtn);
 
-    rockBtn.addEventListener('click', rockSelection = () => {
 
-        let playerChoice = 'rock';
-        let matchResult = (playRound(playerChoice, computerChoice(1, 4)));
-
-        printResults(matchResult);
-        
-    });
-
-    paperBtn.addEventListener('click', rockSelection = () => {
-        let playerChoice = 'paper';
-        console.log(playRound(playerChoice, computerChoice(1, 4)));
-    });
-
-    scissorsBtn.addEventListener('click', rockSelection = () => {
-        let playerChoice = 'scissors';
-        console.log(playRound(playerChoice, computerChoice(1, 4)));
-    });
-
-
-    
 }
 
 function createScoreBoard(){
-}
-function game() {
 
-    createPlayerOptions();
+    let compScore = 0;
+    let playScore = 0;
+
+    let scoreBoard = document.createElement('div');
+    scoreBoard.id = 'scoreboard';
+
+    let resultsHead = document.createElement('h3');
+    resultsHead.id = 'results-head';
+    resultsHead.innerText = 'Match Results';
+    resultsHead.style.textDecoration = "underline";
+
+    let computerScore = document.createElement('p');
+    computerScore.innerText = "Computer Wins: "
+
+    let compScoreCount = document.createElement('p');
+    compScoreCount.innerText = compScore;
+    compScoreCount.id ="comp-counter";
+
+    let playerScore = document.createElement('p');
+    playerScore.innerText = "Player Wins: "
+
+    let playScoreCount = document.createElement('p');
+    playScoreCount.innerText = playScore;
+    playScoreCount.id = "player-counter";
+
+
+    document.body.appendChild(scoreBoard);
+
+    scoreBoard.appendChild(resultsHead);
+
+    scoreBoard.appendChild(computerScore);
+
+    computerScore.appendChild(compScoreCount);
+    
+    scoreBoard.appendChild(playerScore);
+    playerScore.appendChild(playScoreCount);
+
+}
+
+function game() {
 
     createScoreBoard();
 
+    createPlayerOptions();
+
+    let rockBtn = document.getElementById('rock');
+    let paperBtn = document.getElementById('paper');
+    let scissorsBtn = document.getElementById('scissors');
+
+    let compCounter = document.getElementById('comp-counter');
+    let playerCounter = document.getElementById('player-counter');
+
+    let compScore = compCounter.innerText;
+    let playerScore = playerCounter.innerText;
+
+    
+    rockBtn.addEventListener('click', rockSelection = () => {
+
+            let playerChoice = 'rock';
+            let roundResult = (playRound(playerChoice, computerChoice(1, 4)));
+            
+            if (roundResult == 'win'){
+                playerCounter.innerText = playerScore++;               
+            } else if (roundResult == 'loss'){
+                compCounter.innerText = compScore++;
+            } else if (roundResult == 'tie'){
+                console.log("it's a tie");
+            }
+            
+        });
+
+    paperBtn.addEventListener('click', paperSelection = () => {
+            let playerChoice = 'paper';
+            let roundResult = playRound(playerChoice, computerChoice(1, 4));
+            
+            if (roundResult == 'win'){
+                playerCounter.innerText = playerScore++;               
+            } else if (roundResult == 'loss'){
+                compCounter.innerText = compScore++;
+            } else if (roundResult == 'tie'){
+                console.log("it's a tie");
+            }
+        });
+
+    scissorsBtn.addEventListener('click', scissorsSelection = () => {
+            let playerChoice = 'scissors';
+            let roundResult = playRound(playerChoice, computerChoice(1, 4));
+            if (roundResult == 'win'){
+                playerCounter.innerText = playerScore++;               
+            } else if (roundResult == 'loss'){
+                compCounter.innerText = compScore++;
+            } else if (roundResult == 'tie'){
+                console.log("it's a tie");
+            }
+        });
 
 
-
-    //while (gameOver == false) {
-
-
-    //    let matchResult = undefined;
+   // if (playerWins == 5 || computerWins == 5) {
 //
+   //     let winner; 
 //
+   //     if (playerWins == 5) {
+   //         let winner = "Human player";
+   //         console.log("The Game is Over. The " + winner + " wins the game!!" )
+   //         var gameOver = true;
+   //     } else if (computerWins == 5) {
+   //         let winner = "Computer"
+   //         console.log("The Game is Over. The " + winner + " wins the game!!" )
+   //         var gameOver = true;
 //
-    //    if (playerWins == 5 || computerWins == 5) {
-//
-    //        let winner; 
-//
-    //        if (playerWins == 5) {
-    //            let winner = "Human player";
-    //            console.log("The Game is Over. The " + winner + " wins the game!!" )
-    //            var gameOver = true;
-    //        } else if (computerWins == 5) {
-    //            let winner = "Computer"
-    //            console.log("The Game is Over. The " + winner + " wins the game!!" )
-    //            var gameOver = true;
-//
-    //        }
-    //    }
-    //}
+   //     }
+   // }
+    
 }
 
-startButton = document.getElementById('startgame');
 
-console.log(startButton);
+
+
+startButton = document.getElementById('startgame');
 
 
 startButton.addEventListener('click', beginGame = () => 
